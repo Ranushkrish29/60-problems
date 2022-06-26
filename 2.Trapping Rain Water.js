87da1c74796d2d82856aa027b12dfdf31912fdc6
Ranush Krishna <ranush.krishna@gmail.com>
2022-06-26 18:12:20 +0530

    working implementation

--- 2.Trapping Rain Water.js
+++ 2.Trapping Rain Water.js
@@ -24,8 +24,10 @@ Constraints:
     1 <= n <= 2 * 104 
     0 <= height[i] <= 105 */
 
+//--------------------------------------------------------------------------------------------------------------
 
-//Brute Force Implementaion -- > works but time limit is Exceeded:
+
+//Brute Force Implementaion -- > works but time limit is Exceeded: // runtime -> O(n)^2
 function trap(height) {
   let total = 0;
   //loops the height list
@@ -62,6 +64,56 @@ function trap(height) {
   return total;
 }
 console.log(trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]));//6
+
+
+//--------------------------------------------------------------------------------------------------------------
+
+
+//Optimized Implementation (1)   --------> runtime O(n)
+//Runtime: 108 ms, faster than 40.41% of JavaScript online submissions for Trapping Rain Water.
+// Memory Usage: 46.3 MB, less than 12.26 % of JavaScript online submissions for Trapping Rain Water.
+function trap(height) {
+  let total = 0,
+    leftmax = height[0],
+    rightmax = height[height.length - 1],
+    rightbound = {},
+    leftbound = {};
+  //finds the left bound for the every elements in the height list
+  for (let i = 1; i < height.length; i++) {
+    leftmax = leftmax < height[i - 1] ? height[i - 1] : leftmax;
+    //if left bound of the current height is high then
+    if (leftmax > height[i]) {
+      //store the index:value in the leftbound { key-value pairs by using JavaScript Objects }
+      leftbound[i] = leftmax;
+    }
+  }
+  //finds the right bound for the every elements in the height list
+  for (let i = height.length - 2; i >= 0; i--) {
+    rightmax = rightmax < height[i + 1] ? height[i + 1] : rightmax;
+    //if right bound of the current height is high and the leftbound have the current height's left bound in it then 
+    if (rightmax > height[i] && leftbound[i]) {
+      //store the index:value in the rightbound { key-value pairs by using JavaScript Objects }
+      rightbound[i] = rightmax;
+    }
+  }
+  //loops through all the eligible traps index in the rightbound object.
+  for (let i in rightbound) {
+    //formula ->
+    //  find the min from leftbound and rightbound of the eligible trap index.   EX -> 2<3 2 is the min bound
+    let minheightbound = Math.min(leftbound[i], rightbound[i]);
+    //  minheightbound mins the current height of the eligible trap index and adds up with the total.
+    total += minheightbound - height[i];
+  }
+  return total;
+}
+
+console.log(trap([0, 2, 1, 1, 1, 2, 5]));//3
+
+
+
+
+
+
 
 
 
